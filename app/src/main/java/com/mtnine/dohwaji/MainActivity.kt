13e90fc4550bcf2b03f464a1427041ef561136a0
MountainNine <hyunsichang@hanyang.ac.kr>
2022-05-07 17:48:47 +0900

    first commit

---
+++ app/src/main/java/com/mtnine/dohwaji/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mtnine.dohwaji
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.SplashTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
